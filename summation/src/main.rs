6da4808b9052726e70c6d6b9d4e1e673ef3d99bc
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2018-10-11 22:33:54 -0400

    Add summation program in rust.

--- summation/src/main.rs
+++ summation/src/main.rs
@@ -3,7 +3,33 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let numbers = &args[1];
+    let numbers = &args[1..];
 
-    println!("{}", numbers);
+    let numbers: Vec<f32> = numbers
+        .iter()
+        .map(|num| num.parse().unwrap())
+        .collect();
+
+    let summation = summation_with_fold(numbers.clone());
+
+    println!("summation_with_fold is {}", summation);
+
+    let summation = summation_with_loop(numbers.clone());
+
+    println!("summation_with_loop is {}", summation);
+}
+
+fn summation_with_loop(numbers: Vec<f32>) -> f32 {
+    let mut sum = 0.0;
+    for num in numbers.iter() {
+        sum += num;
+    }
+    sum
+}
+
+fn summation_with_fold(numbers: Vec<f32>) -> f32 {
+    let sum = numbers
+        .iter()
+        .fold(0.0, |sum, val| sum + val);
+    return sum;
 }

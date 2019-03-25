5daa54a7e2d8a3d5fe6fd94878f00af7a9462d6f
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2019-03-25 13:25:52 -0400

    Update web scraping file.

---
+++ web_scraping_with_rust/src/lib.rs
@@ -0,0 +1,31 @@
+#[macro_use] extern crate prettytable;
+
+extern crate reqwest;
+extern crate select;
+
+use std::error::Error;
+
+use select::document::Document;
+use select::predicate::Attr;
+use prettytable::Table;
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+    // Web scraping my own blog post :)
+    let res = reqwest::get("https://www.marcelbelmont.com/post/ci-cd-book/").unwrap();
+    assert!(res.status().is_success());
+
+    let document = Document::from_read(res).unwrap();
+
+    let mut table = Table::new();
+
+    let id_selector = "hands-on-continuous-integration-and-delivery";
+    
+    for node in document.find(Attr("id", id_selector)) {
+        let title = node.text();
+        // Fy-> is formatting syntax specific to prettytable
+        table.add_row(row![Fy->title]);
+    }
+    table.printstd();
+
+    Ok(())
+}
